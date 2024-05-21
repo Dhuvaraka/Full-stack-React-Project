86ff4a579901c893b5f0f95f2282237dd76b03ee
Dhuvaraka <211701014@rajalakshmi.edu.in>
2024-05-21 19:00:19 +0530

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html >
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
